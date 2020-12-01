fbef4859013654f2668074f41ca107a86b80d01c
Alex Munda <alex.munda3@gmail.com>
2020-12-01 15:06:11 -0600

    Remove TODO
    Co-authored-by: Andrew Clayton Gray <andrew.gray@hashicorp.com>

--- main.go
+++ main.go
@@ -18,7 +18,6 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: provider.New()}
 
 	if debugMode {
-		// TODO: update this string with the full name of your provider as used in your configs
 		err := plugin.Debug(context.Background(), "registry.terraform.io/hashicorp/hcs", opts)
 		if err != nil {
 			log.Fatal(err.Error())

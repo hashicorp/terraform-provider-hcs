2267dc2f8eb8f4122ce01b8237638e794a886cc4
Andrew Gray <andrew.gray@hashicorp.com>
2020-12-10 14:28:59 -0600

    Implement resource cluster root token create

--- internal/provider/resource_cluster_root_token.go
+++ internal/provider/resource_cluster_root_token.go
@@ -2,13 +2,29 @@ package provider
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/hashicorp/terraform-provider-hcs/internal/clients"
 )
 
 var defaultClusterRootTokenTimeoutDuration = time.Minute * 25
+
+// kubernetesSecretTemplate is the template used to generate a
+// kubernetes formatted secret for the cluster root token.
+const kubernetesSecretTemplate = `apiVersion: v1
+kind: Secret
+metadata:
+name: %s-bootstrap-token
+type: Opaque
+data:
+token: %s`
 
 // resourceClusterRootToken represents the cluster root token resource
 // that is used to bootstrap the cluster's ACL system.
@@ -55,13 +71,49 @@ func resourceClusterRootToken() *schema.Resource {
 
 // resourceClusterRootTokenCreate will generate a new root token for the cluster
 func resourceClusterRootTokenCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
-	// client := meta.(*apiClient)
+	resourceGroupName := d.Get("resource_group_name").(string)
+	managedAppName := d.Get("managed_application_name").(string)
 
-	idFromAPI := "my-id"
-	d.SetId(idFromAPI)
+	managedAppClient := meta.(*clients.Client).ManagedApplication
+	app, err := managedAppClient.Get(ctx, resourceGroupName, managedAppName)
+	if err != nil {
+		return diag.Errorf("failed to check for presence of existing HCS Cluster (Managed Application %q) (Resource Group %q): %+v", managedAppName, resourceGroupName, err)
+	}
+	if app.Response.StatusCode == 404 {
+		// No managed application exists, so this root token should be removed from state
+		log.Printf("[ERROR] no HCS Cluster found for (Managed Application %q) (Resource Group %q)", managedAppName, resourceGroupName)
+		d.SetId("")
+		return nil
+	}
 
-	return diag.Errorf("not implemented")
+	mrgID := *app.ApplicationProperties.ManagedResourceGroupID
+
+	crpClient := meta.(*clients.Client).CustomResourceProvider
+	rootTokenResp, err := crpClient.CreateRootToken(ctx, mrgID)
+	if err != nil {
+		return diag.Errorf("failed to create HCS Cluster root token (Managed Application %q) (Resource Group %q) ID", managedAppName, resourceGroupName)
+	}
+
+	err = d.Set("accessor_id", rootTokenResp.MasterToken.AccessorID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	secretID := rootTokenResp.MasterToken.SecretID
+	err = d.Set("secret_id", secretID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	err = d.Set("kubernetes_secret", generateKubernetesSecret(secretID, managedAppName))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// set the id to the value of the accessor id
+	d.SetId(rootTokenResp.MasterToken.AccessorID)
+
+	return nil
 }
 
 // resourceClusterRootTokenRead will act as a no-op as the root token is not persisted in
@@ -80,4 +132,14 @@ func resourceClusterRootTokenDelete(ctx context.Context, d *schema.ResourceData,
 	// client := meta.(*apiClient)
 
 	return diag.Errorf("not implemented")
+}
+
+// generateKubernetesSecret will generate a Kubernetes secret with
+// a base64 encoded root token secret as it's token.
+func generateKubernetesSecret(rootTokenSecretId, managedAppName string) string {
+	return fmt.Sprintf(kubernetesSecretTemplate,
+		// lowercase the name
+		strings.ToLower(managedAppName),
+		// base64 encode the secret value
+		base64.StdEncoding.EncodeToString([]byte(rootTokenSecretId)))
 }

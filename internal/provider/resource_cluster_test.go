a59ab1dcc46b7b413f977b018270c74a35d5a8df
Andrew Gray <andrew.gray@hashicorp.com>
2020-12-11 15:23:16 -0600

    [HCS-1547] Support Terraform import via AMA ID and cluster name

--- internal/provider/resource_cluster_test.go
+++ internal/provider/resource_cluster_test.go
@@ -1,10 +1,12 @@
 package provider
 
 import (
+	"fmt"
 	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAccResourceScaffolding(t *testing.T) {
@@ -30,3 +32,54 @@ resource "scaffolding_resource" "foo" {
   sample_attribute = "bar"
 }
 `
+
+func Test_validateClusterImportString(t *testing.T) {
+	tcs := []struct {
+		name         string
+		importStr    string
+		managedAppId string
+		clusterName  string
+		err          error
+	}{
+		{
+			name:         "valid import string",
+			importStr:    "/subscriptions/dadbabad-d00d-dada-baad-d00daaaaaaaa/resourceGroups/resource-group/providers/Microsoft.Solutions/applications/app1000:clusterName",
+			managedAppId: "/subscriptions/dadbabad-d00d-dada-baad-d00daaaaaaaa/resourceGroups/resource-group/providers/Microsoft.Solutions/applications/app1000",
+			clusterName:  "clusterName",
+		},
+		{
+			name:      "invalid no colon",
+			importStr: "invalid-no-colon",
+			err:       fmt.Errorf("import id string must be of format `managed_application_id:cluster_name`; id string: %s does not contain `:`", "invalid-no-colon"),
+		},
+		{
+			name:      "invalid more than one colon",
+			importStr: "invalid:multiple:colon",
+			err:       fmt.Errorf("import id string must be of format `managed_application_id:cluster_name`; id string: %s contains more than one `:`", "invalid:multiple:colon"),
+		},
+		{
+			name:      "invalid empty id",
+			importStr: ":cluster_name",
+			err:       fmt.Errorf("import id string must be of format `managed_application_id:cluster_name`; id string: %s has empty string to left of `:`", ":cluster_name"),
+		},
+		{
+			name:      "invalid empty cluster name",
+			importStr: "id:",
+			err:       fmt.Errorf("import id string must be of format `managed_application_id:cluster_name`; id string: %s has empty string to right of `:`", "id:"),
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			r := require.New(t)
+
+			id, name, err := validateClusterImportString(tc.importStr)
+			if tc.err != nil {
+				r.Equal(tc.err, err)
+			} else {
+				r.Equal(tc.managedAppId, id)
+				r.Equal(tc.clusterName, name)
+			}
+		})
+	}
+}
